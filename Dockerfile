a37405792f739fa488743f697ec135fb84b9b15d
Dmitry Ilyin <dima@doty.ru>
2021-12-15 22:06:57 +0300

    Use hack for bufferevent_ssl.h

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ ARG ABI
 ADD arm-openipc-${ABI}_sdk-buildroot.tar.gz /opt
 ADD arm-openipc-${ABI}.cmake /opt/arm-openipc.cmake
 RUN ln -sv /opt/arm-openipc-linux-* /opt/arm-openipc
+ADD bufferevent_ssl.h /tmp/bufferevent_ssl.h
+RUN mv /tmp/bufferevent_ssl.h /opt/arm-openipc/arm-openipc-*/sysroot/usr/include/event2
 ENV PATH /opt/arm-openipc/bin:$PATH
--- arm-openipc-linux-musleabi.cmake
+++ arm-openipc-linux-musleabi.cmake
@@ -7,6 +7,9 @@ set(CMAKE_C_COMPILER arm-openipc-linux-musleabi-gcc CACHE FILEPATH "C compiler")
 set(CMAKE_CXX_COMPILER arm-openipc-linux-musleabi-g++ CACHE FILEPATH "C++ compiler")
 set(CMAKE_ASM_COMPILER arm-openipc-linux-musleabi-gcc CACHE FILEPATH "ASM compiler")
 
+set(CMAKE_SYSROOT
+    /opt/arm-openipc-linux-musleabi_sdk-buildroot/arm-openipc-linux-musleabi/sysroot)
+
 # Search libraries only under *target* paths.
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
